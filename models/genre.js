59daeaecf6124493ed9fb7c4266608c11787f184
agentraghav <raghvendrarathore2@gmail.com>
2021-02-16 11:12:46 +0530

    genre model changes

--- models/genre.js
+++ models/genre.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const GenreSchema = new Schema({
-  title: { type: String, required: true, maxlength: 30 },
+  name: { type: String, required: true, maxlength: 30 },
+  image: { type: String, required: true },
 });
 
 GenreSchema.virtual('url').get(function () {
