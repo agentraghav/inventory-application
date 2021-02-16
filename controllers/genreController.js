9f4fd2fbe02428064f1c6a66815ddbb094d6675d
agentraghav <raghvendrarathore2@gmail.com>
2021-02-16 13:05:40 +0530

    genre form post controller

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,6 +1,6 @@
 const { body, validationResult } = require('express-validator');
 const async = require('async');
-
+const fs = require('fs');
 const Genre = require('../models/genre');
 const Movie = require('../models/movie');
 
@@ -21,4 +21,32 @@ exports.genreList = (req, res, next) => {
 
 exports.genreCreateGet = (req, res, next) => {
   res.render('genre_form');
+};
+
+exports.genreCreatePost = (req, res, next) => {
+  const errors = validationResult(req);
+  const { name } = req.body;
+  const { image } = req.file.path;
+  if (!errors.isEmpty()) {
+    fs.unlink(req.file.path, (err) => {
+      if (err) {
+        return next(err);
+      }
+    });
+    res.render('genre_form', {
+      genre: req.body,
+      errors: errors.array(),
+    });
+  } else {
+    const genre = new Genre({
+      name: name,
+      image: image,
+    });
+    genre.save((error) => {
+      if (error) {
+        return next(error);
+      }
+      res.redirect(genre.url);
+    });
+  }
 };
--- routes/inventory.js
+++ routes/inventory.js
@@ -4,6 +4,18 @@ const multer = require('multer');
 const path = require('path');
 
 const genre_controller = require('../controllers/genreController');
+const genre_validation = require('../validators/genreValidation');
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'public/images');
+  },
+  filename(req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
 
 /* GET home page. */
 
@@ -12,4 +24,12 @@ router.get('/', genre_controller.index);
 router.get('/genres', genre_controller.genreList);
 
 router.get('/genres/create', genre_controller.genreCreateGet);
+
+router.post(
+  '/genres/create',
+  upload.single('image'),
+  genre_validation.genre_form_validation,
+  genre_controller.genreCreatePost
+);
+
 module.exports = router;
