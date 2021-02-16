459b6ec682c973b998bee96946b34f2f1de9966e
agentraghav <raghvendrarathore2@gmail.com>
2021-02-16 10:38:48 +0530

    genre list controller

---
+++ controllers/genreController.js
@@ -0,0 +1,20 @@
+const { body, validationResult } = require('express-validator');
+const async = require('async');
+
+const Genre = require('../models/genre');
+const Movie = require('../models/movie');
+
+exports.index = (req, res, next) => {
+  res.redirect('/genres');
+};
+
+exports.genreList = (req, res, next) => {
+  Genre.find.exec((err, genres) => {
+    if (err) {
+      return next(err);
+    }
+    res.render('genre_list', {
+      genres,
+    });
+  });
+};
